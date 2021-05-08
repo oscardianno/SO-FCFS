92c8ee8c7b8e20cb19cd3e76099d316ef7db106c
oscardianno <oscare.ramirez@alumnos.udg.mx>
2021-05-08 16:08:46 -0500

    Example moving pictureBox

--- SO-FCFS/Form1.Designer.cs
+++ SO-FCFS/Form1.Designer.cs
@@ -29,13 +29,67 @@ namespace SO_FCFS
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.pictureBox_finishLine = new System.Windows.Forms.PictureBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox_finishLine)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 41);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "GO!";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // pictureBox_finishLine
+            // 
+            this.pictureBox_finishLine.Image = global::SO_FCFS.Properties.Resources.finishLine;
+            this.pictureBox_finishLine.Location = new System.Drawing.Point(928, 12);
+            this.pictureBox_finishLine.Name = "pictureBox_finishLine";
+            this.pictureBox_finishLine.Size = new System.Drawing.Size(24, 324);
+            this.pictureBox_finishLine.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox_finishLine.TabIndex = 2;
+            this.pictureBox_finishLine.TabStop = false;
+            this.pictureBox_finishLine.Click += new System.EventHandler(this.pictureBox_finishLine_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::SO_FCFS.Properties.Resources.running_horse;
+            this.pictureBox1.Location = new System.Drawing.Point(102, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(125, 85);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Visible = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(964, 450);
+            this.Controls.Add(this.pictureBox_finishLine);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox_finishLine)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.PictureBox pictureBox_finishLine;
     }
 }
 
